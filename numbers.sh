d4067f6034b4b55a036f9dfcc49bcff7f7f09d9c
Jaxon Skidmore <skidmore@chapman.edu>
2018-11-30 10:24:36 -0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# number.sh
+#Jaxon Skidmore
+echo "Enter a positive number: "
+read number
+N=1      
+while [ $N -le $number ]
+do        
+        if [ $((N%2)) -eq 0 ]
+        then
+                echo $N "is even"
+                N=$[$N+1]
+        else
+                echo $N "is odd"
+                N=$[$N+1] 
+        fi
+done
